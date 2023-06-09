061343197334b99a9a300407194843a9cc79ab8f
Vir <73329256+Vir-8@users.noreply.github.com>
2023-06-09 20:34:46 +0530

    Add and improve settings, stylize layout

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 const bigC = document.querySelector(".bigContainer");
 
 let isMouseDown = false;
-let dimensionSize;
-let boxes;
+let eraserMode = false;
+let rainbowMode = false;
+let gridMode = false;
+
 let newColour = "red";
+let isGridOn;
+let dimensionSize = 16;
+let dimensionInfo = document.querySelector("#dimensionInfo")
 
-const dimensionsButton = document.querySelector(".dimensions");
-const colourButton = document.querySelector(".colour");
+document.documentElement.style.setProperty('--hover-colour', newColour);  
+
+const dimensionSlider = document.querySelector(".dimensions");
+const colourButton = document.querySelector("#colourPicker");
 const gridButton = document.querySelector(".gridLines");
+const clearButton = document.querySelector(".clear");
+const eraserButton = document.querySelector(".eraser");
+const rainbowButton = document.querySelector(".rainbow");
 
+dimensionSlider.addEventListener('input', getNewDimensions);
+colourButton.addEventListener('change', changeColour);
+gridButton.addEventListener('click', toggleGrid);
+clearButton.addEventListener('click', clearDivs);
+eraserButton.addEventListener('click', toggleErase);
+rainbowButton.addEventListener('click', toggleRainbow);
 
-for (let i = 0; i < 256; i++)
-{
-    box = document.createElement('div');
-    box.setAttribute('class', 'grid');
-    bigC.append(box);
-}
+setDimensions(); //generate initial 16x16 grid
 
-
-
-dimensionsButton.addEventListener('click', getNewDimensions);
-colourButton.addEventListener('click', changeColour);
-gridButton.addEventListener('click', gridDisplay);
-
+//change grid dimensions
 function getNewDimensions()
 {
-    dimensionSize = prompt("YO!");
-    changeDimensions();
+    dimensionSize = dimensionSlider.value;
+    console.log(dimensionSize)
+    dimensionInfo.textContent = dimensionSize + "x" + dimensionSize;
+    setDimensions();
 }
 
-
-function changeDimensions()
+function setDimensions()
 {
-    deleteDivs();
+    deleteDivs(); //delete old grid
+    let height = 600/dimensionSize; //determine new box size
 
-    
-    console.log("changing 1");
-    let height = 600/dimensionSize;
-
-
-
+    //generate new grid
     for (let i = 0; i < (dimensionSize*dimensionSize); i++)
     {
         smallC = document.createElement('div');
@@ -47,12 +50,15 @@ function changeDimensions()
         smallC.style.height = height + "px";
         bigC.append(smallC);
     }
-
-
     boxes = document.querySelectorAll('.grid');
-    console.log("changing 2");
 
-    
+    if (isGridOn === true)
+    {
+        boxes.forEach(box => {
+            box.style.border = "1px solid black";
+        });
+    }
+    console.log("changing 2");
 }
 
 function deleteDivs()
@@ -60,61 +66,134 @@ function deleteDivs()
     while ( bigC.firstChild ) bigC.removeChild( bigC.firstChild );
 }
 
-
-
-
-function changeColour()
+//clear divs
+function clearDivs()
 {
-    newColour = color
+    boxes = document.querySelectorAll('.grid')
+    boxes.forEach(box => {
+        box.style.backgroundColor = "";
+    });
 }
 
+//change colour button
+function changeColour()
+{
+    newColour = colourButton.value;
+    document.documentElement.style.setProperty('--hover-colour', newColour);  
+}
 
-function gridDisplay()
+//grid on/off button
+function toggleGrid()
 {
     boxes = document.querySelectorAll('.grid');
-
-
     boxes.forEach(box => {
 
-        if (box.style.border === "none")
+        if (box.style.border === "none" || box.style.border === "")
         {
             box.style.border = "1px solid black";
+            isGridOn = true;
             console.log("NONE");
+            gridButton.style.opacity = 0.5;
+            gridButton.textContent = "Grid: Enabled";
         }
         else
         {
             box.style.border = "none";
+            isGridOn = false;
+            gridButton.style.opacity = 1;
+            gridButton.textContent = "Grid: Disabled";
         }
     });
-
     console.log("border");
-    
 }
 
+//erase stuff
+function toggleErase()
+{
+    if (eraserMode === false) 
+    {
+        oldColour = newColour;
+        newColour = "";
+        eraserMode = true;
+        eraserButton.style.opacity = 0.5;
+        eraserButton.textContent = "Eraser: Enabled"
+    }
+    else 
+    {
+        newColour = oldColour;
+        eraserMode = false;
+        eraserButton.style.opacity = 1;
+        eraserButton.textContent = "Eraser: Disabled";
+    }    
+}
 
-boxes = document.querySelectorAll('.grid');
- 
+//rainbow mode
+function toggleRainbow()
+{
+    if (rainbowMode === false)
+    {
+        rainbowMode = true;
+        rainbowButton.style.opacity = 0.5;
+        rainbowButton.textContent = "Rainbow: Enabled";
+    }
+    else
+    {
+        rainbowMode = false;
+        rainbowButton.style.opacity = 1;
+        rainbowButton.textContent = "Rainbow: Disabled";
+        document.documentElement.style.setProperty('--hover-colour', newColour);
+    }
+}
+
+function getRandomColour() 
+{
+    var letters = '0123456789ABCDEF';
+    var colour = '#';
+    for (var i = 0; i < 6; i++) 
+    {
+      colour += letters[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+}
+
+//paint stuff
 bigC.addEventListener('mouseup', function() {
     isMouseDown = false;
 });
 
-bigC.addEventListener("mousedown", function(event) {
+bigC.addEventListener('mouseleave', function() {
+    isMouseDown = false;
+});
+
+bigC.addEventListener('mousedown', function(event) {
     event.preventDefault(); // Disable default drag behavior
     isMouseDown = true;
-
     let target = event.target;
   
-    if (target.classList.contains('grid')) 
+    if ((target.classList.contains('grid') && rainbowMode === false) || eraserMode === true) 
     {
         target.style.backgroundColor = newColour;
         console.log('mouse down box');
     }
+    else if (target.classList.contains('grid') && rainbowMode === true && eraserMode === false)  
+    {
+        rainbowColour = getRandomColour();
+        target.style.backgroundColor = rainbowColour;
+        document.documentElement.style.setProperty('--hover-colour', rainbowColour);
+        console.log('mouse down box');
+    }
 });
 
+bigC.addEventListener('mouseover', function(event) {
 
-  
-  bigC.addEventListener("mouseover", function(event) {
-    if (isMouseDown && event.target.classList.contains("grid")) {
-      event.target.style.backgroundColor = newColour;
+    if (isMouseDown && ((event.target.classList.contains("grid") && rainbowMode === false) || eraserMode === true)) 
+    {
+        event.target.style.backgroundColor = newColour;
     }
-  });
+    else if (isMouseDown && event.target.classList.contains("grid") && rainbowMode === true && eraserMode === false) 
+    {
+        rainbowColour = getRandomColour();
+        event.target.style.backgroundColor = rainbowColour;
+        document.documentElement.style.setProperty('--hover-colour', rainbowColour);
+    }
+});
